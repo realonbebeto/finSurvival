536e4391497082bd4fce4e0358a74483a882e737
realonbebeto <projobs254@gmail.com>
2023-03-20 19:02:14 +0300

    profile schema change

--- app/gateway/gateway/models/profile.py
+++ app/gateway/gateway/models/profile.py
@@ -25,17 +25,18 @@ class Profile(Base):
     outstanding_debt = Column(Float, nullable=False, unique=False)
     credit_utilization_ratio = Column(Float, nullable=False, unique=False)
     credit_history_age = Column(Float, nullable=False, unique=False)
-    payment_of_min_amount = Column(Float, nullable=False, unique=False)
+    payment_of_min_amount = Column(String, nullable=False, unique=False)
     total_emi_per_month = Column(Float, nullable=False, unique=False)
     amount_invested_monthly = Column(Float, nullable=False, unique=False)
     payment_behaviour = Column(String, nullable=False, unique=False)
     monthly_balance = Column(Float, nullable=False, unique=False)
     # other attributes
-    times = Column(ARRAY(Integer), nullable=True, unique=False)
-    hazard_score = Column(ARRAY(Integer), nullable=True, unique=False)
-    risk_score = Column(ARRAY(Integer), nullable=True, unique=False)
-    survival_score = Column(ARRAY(Integer), nullable=True, unique=False)
+    times = Column(ARRAY(Float), nullable=True, unique=False)
+    hazard_score = Column(ARRAY(Float), nullable=True, unique=False)
+    risk_score = Column(ARRAY(Float), nullable=True, unique=False)
+    survival_score = Column(ARRAY(Float), nullable=True, unique=False)
     created_at = Column(
         TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
-    updated_at = Column(TIMESTAMP(timezone=True), onupdate=datetime.now(timezone.utc))
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        onupdate=datetime.now(timezone.utc))
